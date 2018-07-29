50546c28c24290183db6ef14bb27ca406173ca81
DiegoP <diegowall39@gmail.com>
2018-07-29 17:39:26 -0600

    Archivos importante para el tema

--- index.php
+++ index.php
@@ -1,79 +1,93 @@
-    <!DOCTYPE html>
-        <html lang="es">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Web</title>
-            <link rel="stylesheet" href="http://localhost/Prueba/wp-content/themes/theme-web/assets/css/bootstrap.min.css">
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
-            <link rel="stylesheet" href="http://localhost/Prueba/wp-content/themes/theme-web/assets/css/tech.css">
-        </head>
-        <body>
-        <header class="row header">
-        <section class="col-6 header__main">
-        <section class="col-12 header__logo">
-                <figure class="header__logo__img e-center">
-                <img src="http://localhost/Prueba/wp-content/themes/theme-web/assets/img/viral.png" alt="Logo Web">
-                </figure>
-            </section>
-        <nav class="col-12 header__nav">
-            <ul class="nav nav__main j-center">
-        <li class="nav-item nav__item">
-            <a class="nav-link t-black " href="#">Tecnologia</a>
-        </li>
-        <li class="nav-item nav__main">
-            <a class="nav-link t-black" href="#">Musica</a>
-        </li>
-        <li class="nav-item nav__main">
-            <a class="nav-link t-black" href="#">Leeks</a>
-        </li>
-        <li class="nav-item nav__main">
-            <a class="nav-link t-black disabled" href="#">Proximamente</a>
-        </li>
-        </ul>
-            </nav>
-            </section>
-        <section class="col-6 header__sec d-flex">
-            <div class="row full-width">
-                <nav class="col-6=">
+            <!DOCTYPE html>
+                <html lang="es">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <title>Web</title>
+                    <link rel="stylesheet" href="http://localhost/Prueba/wp-content/themes/theme-web/assets/css/bootstrap.min.css">
+                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+                    <link rel="stylesheet" href="http://localhost/Prueba/wp-content/themes/theme-web/assets/css/tech.css">
+                </head>
+                <body>
+                <header class="row header">
+                <section class="col-6 header__main">
+                <section class="col-12 header__logo">
+                        <figure class="header__logo__img e-center">
+                        <img src="http://localhost/Prueba/wp-content/themes/theme-web/assets/img/viral.png" alt="Logo Web">
+                        </figure>
+                    </section>
                 <nav class="col-12 header__nav">
-            <ul class="nav nav__main">
-        <li class="nav-item nav__item">
-            <a class="nav-link active" href="#">
-            <i class="t-black fab fa-facebook-square"></i>
-            </a>
-        </li>
-        <li class="nav-item nav__main">
-            <a class="nav-link" href="#">
-            <i class="t-black fab fa-instagram"></i>
-            </a>
-        </li>
-        <li class="nav-item nav__main">
-            <a class="nav-link" href="#">
-            <i class="t-black fab fa-twitter"></i>
-            </a>
-        </li>
-        <li class="nav-item nav__main">
-            <a class="nav-link " href="#">
-            <i class="t-black fab fa-youtube"></i>
-            </a>
-        </li>
-        </ul> 
-</div>
-        </section>
-        <nav class="col-10">
-        </nav>
-        <section class="search__input col-2">
-        <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-        <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2">
-            <i class="fas fa-search"></i>
-            </button>
+                    <ul class="nav nav__main j-center">
+                <li class="nav-item nav__item">
+                    <a class="nav-link t-black " href="#">Tecnologia</a>
+                </li>
+                <li class="nav-item nav__main">
+                    <a class="nav-link t-black" href="#">Musica</a>
+                </li>
+                <li class="nav-item nav__main">
+                    <a class="nav-link t-black" href="#">Leeks</a>
+                </li>
+                <li class="nav-item nav__main">
+                    <a class="nav-link t-black disabled" href="#">Proximamente</a>
+                </li>
+                </ul>
+                    </nav>
+                    </section>
+                <section class="col-6 header__sec d-flex">
+                    <div class="row full-width">
+                        <nav class="col-6=">
+                        <nav class="col-12 header__nav">
+                    <ul class="nav nav__main">
+                <li class="nav-item nav__item">
+                    <a class="nav-link active" href="#">
+                    <i class="t-black fab fa-facebook-square"></i>
+                    </a>
+                </li>
+                <li class="nav-item nav__main">
+                    <a class="nav-link" href="#">
+                    <i class="t-black fab fa-instagram"></i>
+                    </a>
+                </li>
+                <li class="nav-item nav__main">
+                    <a class="nav-link" href="#">
+                    <i class="t-black fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li class="nav-item nav__main">
+                    <a class="nav-link " href="#">
+                    <i class="t-black fab fa-youtube"></i>
+                    </a>
+                </li>
+                </ul> 
         </div>
-        </div>
-        </section>
-            </header>
-        </body>
-        </html>
+                </section>
+                <nav class="col-10">
+                </nav>
+                <section class="search__input col-2">
+                <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+                    <i class="fas fa-search"></i>
+                    </button>
+                </div>
+                </div>
+                </section>
+                    </header>
+                    <br>
+
+                    <!-- Footer -->
+    <footer class="page-footer">
+    <figure class="footer__logo "> 
+        
+    <img src="http://localhost/Prueba/wp-content/themes/theme-web/assets/img/viral2.png" alt="">
+    </figure>
+
+
+
+    </footer>
+    <!-- Footer -->
+        
+                </body>
+                </html>
